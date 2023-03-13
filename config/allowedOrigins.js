ee5f3019d5cea5dec7d7b4e32258231ead9f66af
Piotr Przybyła <piotrprz03@gmail.com>
2023-03-13 12:42:51 +0100

    Update Cors options

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:5000", "http://localhost:3000"];
+const allowedOrigins = ["http://localhost:5000", "http://localhost:3000", "https://pizza-crud-frontend.onrender.com"];
 
 module.exports = allowedOrigins;
